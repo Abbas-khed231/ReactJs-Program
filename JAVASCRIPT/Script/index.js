f7b752fd834f092d21576249bd77253e7192c5f6
Abbas-khed231 <khedabbas@gmail.com>
2023-03-02 14:15:12 +0530

    All HTML,CSS and JAVASCRIPT files

---
+++ JAVASCRIPT/Script/index.js
@@ -0,0 +1,11 @@
+var f_name = "Abbas";
+var age = 18;
+var weight = 64.5;
+var height = 5.10;
+var react_completion = false;
+
+console.log(f_name);
+console.log(age);
+console.log(weight);
+console.log(height);
+console.log(react_completion);
